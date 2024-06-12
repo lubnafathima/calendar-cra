3b155fd50a30199c50ace55b2854b0d6a89eeda5
Lubna Fathima N <lubnafathima275@gmail.com>
2024-06-12 20:42:45 +0530

    added calendar

---
+++ src/components/Calendar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  DAY_OF_THE_WEEK,
+  CURRENT_DATE,
+  CURRENT_MONTH,
+  CURRENT_YEAR,
+} from "../utils/constants";
+
+export default function Calendar({ month, year }) {
+  const getLastDate = (monthValue, yearValue) => {
+    return new Date(yearValue, monthValue + 1, 0).getDate();
+  };
+
+  let firstDateOfAMonth = 1;
+  const lastDateOfMonth = getLastDate(month, year);
+  const firstDayOfAMonth = new Date(year, month, firstDateOfAMonth).getDay();
+
+  const items = [];
+
+  for (let i = 0; i < firstDayOfAMonth; i++) {
+    items.push(<div key={i} className="calendar_day empty"></div>);
+  }
+
+  for (let day = 1; day <= lastDateOfMonth; day++) {
+    const isToday =
+      day === CURRENT_DATE && month === CURRENT_MONTH && year === CURRENT_YEAR;
+    items.push(
+      <div key={day} className={`calendar_day ${isToday ? "today" : ""}`}>
+        {day}
+      </div>
+    );
+  }
+
+  return (
+    <div className="calendar">
+      <div className="calendar_header">
+        {DAY_OF_THE_WEEK.map((day, index) => (
+          <p key={index} className="day_of_week">
+            {day}
+          </p>
+        ))}
+      </div>
+      <div className="calendar_main">{items}</div>
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { MONTHS, CURRENT_MONTH, CURRENT_YEAR} from "../utils/constants";
+
+export default function Header({ month, year, setMonth, setYear }) {
+  const handlePreviousMonth = () => {
+    if (month === 0) {
+      setMonth(11);
+      setYear(year - 1);
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
+  const handleNextMonth = () => {
+    if (month === 11) {
+      setMonth(0);
+      setYear(year + 1);
+    } else {
+      setMonth(month + 1);
+    }
+  };
+
+  const handleToday = () => {
+    setMonth(CURRENT_MONTH);
+    setYear(CURRENT_YEAR);
+  }
+
+  return (
+    <div className="header">
+      <button className="button" onClick={handleToday}>Today</button>
+      <h2 className="header_date">
+        {MONTHS[month]}, {year}
+      </h2>
+      <div className="header_button_group">
+        <button className="button" onClick={handlePreviousMonth}>
+          <i className="fa-solid fa-arrow-left"></i>
+        </button>
+        <button className="button" onClick={handleNextMonth}>
+          <i className="fa-solid fa-arrow-right"></i>
+        </button>
+      </div>
+    </div>
+  );
+}
